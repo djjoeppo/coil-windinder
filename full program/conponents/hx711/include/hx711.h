2e952375d4ca3cd4eb55761897464aded83fa065
djjoeppo <134445212+djjoeppo@users.noreply.github.com>
2026-07-26 13:28:13 +0200

    Add files via upload

---
+++ full program/conponents/hx711/include/hx711.h
@@ -0,0 +1,23 @@
+#ifndef HX711_H
+#define HX711_H
+
+#include <stdint.h>
+#include <stdbool.h>
+
+typedef struct {
+    int sck_pin;
+    int dt_pin;
+    long offset;
+    float divider;
+} hx711_config_t;
+
+void hx711_init(hx711_config_t *config);
+long hx711_read_raw(void);
+void hx711_tare(int samples);
+float hx711_get_weight(void);
+bool hx711_is_online(void);
+
+// Task for background updates
+void hx711_update_task(void *pvParameters);
+
+#endif // HX711_H
